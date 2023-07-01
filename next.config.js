80f85e32567a97f202ee2a65d37a95be14f91d57
Ankit Singh <singhankit25apr@gmail.com>
2023-07-01 15:35:13 +0530

    .

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      'picsum.photos',
+      'loremflickr.com',
+      'cloudflare-ipfs.com',
+      'miro.medium.com',
+      'novu.co',
+    ],
+  },
 };
 
 module.exports = nextConfig;
